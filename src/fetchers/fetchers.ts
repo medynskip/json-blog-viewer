1d7bd78853fcbbdc96a9e3736a0a709c9dd7066c
Piotr Medynski <medynski@outlook.com>
2024-07-23 15:26:00 +0200

    feat: initial commit and simple api fetch

---
+++ src/fetchers/fetchers.ts
@@ -0,0 +1,17 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/';
+
+export async function fetchAPI<T>(
+  apiPath: string,
+): Promise<T> {
+  try {
+    const requestUrl = `${API_URL}/${apiPath}`
+    const response = await fetch(requestUrl);
+
+    const data = (await response.json()) as T;
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
